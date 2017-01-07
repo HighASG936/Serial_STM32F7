f38a1512d36afd8edc6fdcd98ddfe67c5f7f50c9
HighASG936 <lz_rock@hotmail.com>
2017-01-06 21:05:12 -0600

    Capacidad de borrar Caracteres en la terminal

--- Serial.h
+++ Serial.h
@@ -2,14 +2,49 @@
 #define SERIALSOFTWARE_H
 
 #include "stm32f7xx_hal.h"
+#include "ANSI_VT100.h"
 
 #define SIZE_BUFFER_ATENCION	100
 #define SIZE_BUFFER_RESPUESTA 100
+
+#pragma anon_unions
 
 uint8_t BufferAtencion[SIZE_BUFFER_ATENCION];
 uint8_t	BufferRespuesta[SIZE_BUFFER_RESPUESTA];
 
 extern UART_HandleTypeDef huart7;
+
+typedef union
+{
+	uint8_t all;
+	struct
+	{
+		uint8_t bit7   :1;
+		uint8_t bit6	 :1;
+		uint8_t bit5	 :1;
+		uint8_t bit4	 :1;
+		uint8_t bit3	 :1;
+		uint8_t bit2	 :1;
+		uint8_t bit1	 :1;
+		uint8_t bit0	 :1;
+	};
+
+}uFlags;
+
+
+typedef struct
+{
+	uFlags Flags;
+}eSerial;
+
+eSerial gsSerial;
+
+void Serial_Iniciar(void)
+{
+	printf("%s", HEADER);
+
+}
+
 
 void Serial_EnviarComando(void)
 {
@@ -29,28 +64,32 @@ void Serial_EnviarComando(void)
 
 void Serial_Atencion(void)
 {
-	static uint8_t i = 0;
-	if(i>SIZE_BUFFER_ATENCION) return;
+	static uint8_t SerialAtencionIndice = 0;
+	
+	if(SerialAtencionIndice>SIZE_BUFFER_ATENCION) return;
 	int32_t StatusReceiveChar = ITM_ReceiveChar();
-	if(StatusReceiveChar == 0x08)
-	{
-		BufferAtencion[i] = 0x00;
-		i--;
-		printf("%c%c%c%c",0x1B,'[','D',0x08);
-	}
 	if(StatusReceiveChar == -1) return; 
-	BufferAtencion[i] = StatusReceiveChar;
+	if(StatusReceiveChar == ESC)return;
+	if(StatusReceiveChar == 0x08)
+	{	
+		if(SerialAtencionIndice==0) return;
+		SerialAtencionIndice--;
+		BufferAtencion[SerialAtencionIndice] = 0x00;
+		printf("%s %s", CURSOR_IZQUIERDA,CURSOR_IZQUIERDA);
+		return;
+	}
+	BufferAtencion[SerialAtencionIndice] = StatusReceiveChar;
 	if(StatusReceiveChar == 0x0d)
 		{
-			BufferAtencion[i+1] = 0x0a;
-			BufferAtencion[i+2] = 0x00;
+			BufferAtencion[SerialAtencionIndice+1] = 0x0a;
+			BufferAtencion[SerialAtencionIndice+2] = 0x00;
 			printf("\r\n");
-			i = 0;
+			SerialAtencionIndice = 0;
 			Serial_EnviarComando();
 			return;
 		}
-	printf("%c",  BufferAtencion[i]);
-	i++;
+	printf("%c",  BufferAtencion[SerialAtencionIndice]);
+	SerialAtencionIndice++;
 }
 
 #endif
